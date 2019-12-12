52fc00ed7adb60b7565289b18b4f5a1708eac7b0
thetisrock <torok.chris@gmail.com>
2019-12-11 20:40:39 -0500

    finished lesson on urls.py & views.py w/ templates.

--- my_app/todo_list/urls.py
+++ my_app/todo_list/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from todo_list import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # admin
-    path('', include('todo_list.urls')),  # root
+	path('', views.home, name='home'),
+	path('about', views.about, name='about'),
 ]
