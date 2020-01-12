826b5980698a377f69e93062c259c020011bbfe7
thetisrock <torok.chris@gmail.com>
2020-01-12 15:39:48 -0500

    changed db schema code-first

--- my_app/todo_list/models.py
+++ my_app/todo_list/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class ListItem(models.Model):
     item = models.CharField(max_length=150)
     complete = models.BooleanField(default=False)
-    of_list = models.CharField(max_length=50)
 
 
     def __str__(self):
