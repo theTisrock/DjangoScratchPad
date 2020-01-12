35340864afe5f11383b5e2c048e38f29f96b628c
thetisrock <torok.chris@gmail.com>
2020-01-12 15:41:20 -0500

    add a form for adding to db

--- my_app/todo_list/forms.py
+++ my_app/todo_list/forms.py
@@ -5,6 +5,6 @@ from .models import ListItem
 class ListItemForm(forms.ModelForm):
     class Meta:
         model = ListItem
-        fields = ['item', 'complete', 'of_list']
+        fields = ['item', 'complete']
 
 # end
