b3413354118c7ac0026df0dec370cc1b7947d183
thetisrock <torok.chris@gmail.com>
2020-01-11 20:26:55 -0500

    after python manage.py makemigrations command

--- my_app/todo_list/models.py
+++ my_app/todo_list/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class List(models.Model):
+class ListItem(models.Model):
     item = models.CharField(max_length=150)
     complete = models.BooleanField(default=False)
+    of_list = models.CharField(max_length=50)
+
 
     def __str__(self):
         return self.item
