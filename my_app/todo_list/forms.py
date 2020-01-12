ad51de829cd2a8a7d7941819459c5aaa0ca18e6e
thetisrock <torok.chris@gmail.com>
2020-01-12 14:46:18 -0500

    added ListItem class to forms

---
+++ my_app/todo_list/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import ListItem
+
+
+class ListItemForm(forms.ModelForm):
+    class Meta:
+        model = ListItem
+        fields = ['item', 'complete', 'of_list']
+
+# end
