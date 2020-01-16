90e480367bc3fe6a5788339d6f93fdf3f4cf9bf9
thetisrock <torok.chris@gmail.com>
2020-01-15 22:40:41 -0500

    ensure model property is present

--- my_app/todo_list/models.py
+++ my_app/todo_list/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class ListItem(models.Model):
     item = models.CharField(max_length=150)
-    complete = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)  # model has proper component for implementation of 'toggle'
 
 
     def __str__(self):
