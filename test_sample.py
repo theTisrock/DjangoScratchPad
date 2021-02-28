55a04438e88df5c9d7070f88d3c919bdff59aef8
chris torok <torok.chris@gmail.com>
2021-02-28 13:26:43 -0500

    setup a test class

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,15 @@
 
+
 def increment(number):
     return number + 1
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
 
 
 def test_passes():
@@ -9,3 +18,15 @@ def test_passes():
 
 def test_fails():
     assert increment(5) == 0
+
+
+class TestMath:
+
+    def test_increment(self):
+        assert increment(5) == 6  # pass
+
+    def test_add(self):
+        assert add(1, 1) == 10  # fail
+
+    def test_subtract(self):
+        assert subtract(10, 9) == 1
